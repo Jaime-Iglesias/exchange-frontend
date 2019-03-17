7e41214896a05f89e570f8c0618d8ea246506ab0
Jaime Iglesias <jaime.i.botas@gmail.com>
2019-03-17 21:02:03 +0100

    working on metamask update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import Web3 from 'web3';
+import React, { Component } from 'react'
+import './App.css'
 
-import Header from "./components/Header";
+import Header from "./components/Header"
+import getWeb3 from './components/getWeb3'
 import Exchange from './contracts/MyExchange.json'
 import TestingToken from './contracts/TestingToken.json'
 import MyExchange from './components/MyExchange'
@@ -12,64 +12,54 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state ={
-            provider: null,
+            web3: null,
             network: -1,
-            address: '',
-            contract: null,
-            owner: '',
+            account: '',
+            dapp: null,
             token: null,
             loaded: false,
         }
     }
 
     componentWillMount() {
-        this.loadBlockchainData();
+        this.loadBlockchainData()
     }
 
     async loadBlockchainData() {
-        const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
-        console.log(web3)
-        const network = await web3.eth.net.getId()
-        const accounts = await web3.eth.getAccounts()
-        const exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[network].address)
-        this.setState({ provider: web3, network, address: accounts[0], contract: exchange })
-        const owner = await exchange.methods.owner().call()
-        this.setState({ owner })
-        const token = new web3.eth.Contract(TestingToken.abi, TestingToken.networks[network].address)
-        this.setState({ token })
-        const totalSupply = await token.methods.totalSupply().call()
-        this.setState({ loaded: true })
-
-
-        console.log('provider', web3)
-        console.log('network:', network)
-        console.log('account:', accounts[0])
-        console.log('contract:', exchange)
-        console.log('token:', token)
-        console.log('totalSupply', totalSupply)
-    }
-
-    balanceForToken() {
-        const {
-            address,
-            contract,
-            token,
-        } = this.state;
-        MyExchange.getUserBalanceForToken(address, contract, token.options.address)
+        const expectedNetwork = 5777
+        try {
+            const web3 = await getWeb3()
+            const network = await web3.eth.net.getId()
+            const accounts = await web3.eth.getAccounts()
+            if (network === expectedNetwork) {
+                const exchangeContract = new web3.eth.Contract(Exchange.abi, Exchange.networks[network].address)
+                const tokenContract = new web3.eth.Contract(TestingToken.abi, TestingToken.networks[network].address)
+                this.setState( {
+                    web3,
+                    network,
+                    account: accounts[0],
+                    dapp: exchangeContract,
+                    token: tokenContract,
+                    loaded: true,
+                })
+            } else {
+                console.log("Current version of this app is only supported by a local network, please run a local Ganache to use it")
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     renderApp() {
         if (this.state.loaded === false) {
-            return;
+            return ( <p> Loading... </p> )
         } else {
             return(
                 <div>
                     <Header/>
                     <h1>  Hello World!  </h1>
-                    <p> Your account: { this.state.address } </p>
+                    <p> Your account: { this.state.account } </p>
                     <p> The network: { this.state.network } </p>
-                    <p> Contract owner: { this.state.owner }</p>
-                    <p> User balance for Token: { this.balanceForToken() }</p>
                 </div>
             )
         }
@@ -78,8 +68,8 @@ class App extends Component {
     render() {
         return(
           <div> { this.renderApp() } </div>
-        );
+        )
     }
 }
 
-export default App;
+export default App
